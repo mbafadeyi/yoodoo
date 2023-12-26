a2e0729763bafddaadfb79776cd075e88ae76335
M.B.A. Fadeyi <fadeyibajide@gmail.com>
2023-12-26 22:34:13 +0000

    Project setup

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  console.log("hello");
+  return (
+    <div>
+      {" "}
+      ©2023 Yoodoo. All rights reserved.
+      <div></div>
+    </div>
+  );
+};
+
+export default Footer;
