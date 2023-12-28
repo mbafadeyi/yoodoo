0570e29f88dd9d9f7b3bc858d04057a82cb675db
M.B.A. Fadeyi <fadeyibajide@gmail.com>
2023-12-28 20:22:48 +0000

    Sign in with Email and Password

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,4 +1,8 @@
+import User from "@/models/User";
+import connect from "@/utils/db";
+import bcrypt from "bcryptjs";
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
@@ -7,7 +11,37 @@ const handler = NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      async authorize(credentials) {
+        await connect();
+
+        try {
+          const user = await User.findOne({ email: credentials.email });
+
+          if (user) {
+            //Check password
+            const isPasswordCorrect = await bcrypt.compare(
+              credentials.password,
+              user.password,
+            );
+
+            if (isPasswordCorrect) {
+              return user;
+            } else {
+              throw new Error("Wrong Credentials!");
+            }
+          } else {
+            throw new Error("User not found!");
+          }
+        } catch (err) {
+          throw new Error(err);
+        }
+      },
+    }),
   ],
+  pages: { error: "/dashboard/login" },
 });
 
 export { handler as GET, handler as POST };
